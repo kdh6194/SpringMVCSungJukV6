20d8f893a6db450ce60554fd62d844ba736b5478
kdh6194 <kdh6194@naver.com>
2023-04-26 13:06:47 +0900

    스프링MVC - 성적 저장2

--- src/main/java/honeybee/spring4/sungjukv6/controller/SungJukController.java
+++ src/main/java/honeybee/spring4/sungjukv6/controller/SungJukController.java
@@ -1,13 +1,11 @@
 package honeybee.spring4.sungjukv6.controller;
 
+import honeybee.spring4.sungjukv6.model.SungJukVO;
 import honeybee.spring4.sungjukv6.service.SungJukV6Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -25,6 +23,22 @@ public class SungJukController {
         //sungjuklist.jsp에 성적조회결과 sjs라는 이름으로 넘김
         mv.addObject("sjs",sjsrv.readSungJuk());
         mv.setViewName("sungjuklist"); // 뷰 이름 지정
+
+        return mv;
+    }
+
+    // 성적 입력 폼 표시
+    @GetMapping("/add")
+    public String add() {
+        return "sungjuk";
+    }
+    // 성적 입력 처리
+    @PostMapping("/add")
+    public ModelAndView addok(SungJukVO sj) {
+        ModelAndView mv = new ModelAndView();
+
+        mv.addObject("sj",sj);
+        mv.setViewName("sungjukok");
 
         return mv;
     }
---
+++ src/main/webapp/WEB-INF/views/sungjukok.jsp
@@ -0,0 +1,14 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>성적처리 프로그램 v6</title>
+</head>
+<body>
+<h1>성적처리 프로그램 v6</h1>
+
+<p>${sj}</p>
+
+</body>
+</html>
